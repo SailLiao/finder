c2aac4a698166359a2bfe43ed3dfbf540daac292
lzflovehx <1065500519@qq.com>
2016-05-30 15:44:01 +0800

    this is my first commit

---
+++ src/main/java/com/lzf/finder/IndexController.java
@@ -0,0 +1,23 @@
+package com.lzf.finder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("index")
+public class IndexController {
+
+	private static final String INDEX = "index";
+	private static final String GETSTARTED = "/getting-started";
+
+	@RequestMapping
+	public String login() {
+		return INDEX;
+	}
+
+	@RequestMapping("getStarted")
+	public String getStarted() {
+		return GETSTARTED;
+	}
+
+}
